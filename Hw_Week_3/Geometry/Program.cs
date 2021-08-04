be6cd13bb71e7d9582d3c3801cf213c3be6b67dd
Bato_gt05@itstepru.onmicrosoft.com <Bato_gt05@itstepru.onmicrosoft.com>
2021-08-04 08:07:45 +0300

    Figure

--- Hw_Week_3/Geometry/Program.cs
+++ Hw_Week_3/Geometry/Program.cs
@@ -6,7 +6,18 @@ namespace Geometry
     {
         static void Main(string[] args)
         {
-             
+            Circle circle = new Circle(3.5);
+            Console.WriteLine("Circle:");
+            Console.WriteLine($"Площадь: {circle.CalcArea()}, Периметр: {circle.CalcPerimeter()}");
+
+            Square square = new Square(4);
+            Console.WriteLine("Square:");
+            Console.WriteLine($"Площадь: {square.CalcArea()}, Периметр: {square.CalcPerimeter()}");
+
+            Triangle triangle = new Triangle(3, 4, 5);
+            Console.WriteLine("Triangle:");
+            Console.WriteLine($"Площадь: {triangle.CalcArea()}, Периметр: {triangle.CalcPerimeter()}");
+
         }
     }
 }
