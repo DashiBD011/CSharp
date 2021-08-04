8776a437ecd5f1aa07e0564e414f1e7c10c60a9a
Bato_gt05@itstepru.onmicrosoft.com <Bato_gt05@itstepru.onmicrosoft.com>
2021-08-04 10:12:12 +0300

    Pencil

--- CW_Week3/Pencil/Program.cs
+++ CW_Week3/Pencil/Program.cs
@@ -8,10 +8,23 @@ namespace Pencil
        
         static void Main()
         {         
-            Pencil pencil = new Pencil(170,"wood",Condition.broken,Colour.Black,false,3.5);
-            pencil.Info();
-           
+            //Pencil pencil = new Pencil(170,"wood",Condition.broken,Colour.Black,false,3.5);
+            //pencil.Info();
 
+            Pencil[] nabor =
+            {
+                new Pencil(170,"wood",Condition.sharpened,Colour.Red,false,5),
+                new Pencil(170,"wood",Condition.sharpened,Colour.Blue,false,5),
+                new Pencil(170,"wood",Condition.blunt,Colour.Gray,true,3.5),
+                new Pencil(170,"wood",Condition.broken,Colour.Yellow,false,0),
+                new Pencil(170,"wood",Condition.broken,Colour.Green,false,0)
+            };
+
+            foreach (var pencil in nabor)
+            {
+                pencil.Info();
+                Console.WriteLine();
+            }
         }
        
     }
